9114204792e71f285c3d14526dcb6b9e129b7c0d
= <=>
2023-01-28 22:16:29 -0300

    TwoSum completed

---
+++ Problems/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CSharp.Problems
+{
+    public class Solution { 
+    
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int > hash = new Dictionary<int, int>();
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hash.ContainsValue(target - nums[i]))
+                {
+                    result[0] = i;
+                    result[1] = hash.FirstOrDefault(x => x.Value == (target - nums[i])).Key;              
+                }
+                hash.Add(i, nums[i]);
+            }
+            return result;
+        }
+    }
+}
